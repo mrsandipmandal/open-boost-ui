fe26ca9ccf4260e36edcd8445306e434b2b76632
Sandip Mandal <sandiptaibichara@gmail.com>
2025-12-08 12:51:57 +0530

    Enhance select component with dynamic theming and improved class structure

--- resources/views/components/flash/select.blade.php
+++ resources/views/components/flash/select.blade.php
@@ -3,6 +3,7 @@
     'multiple' => $multiple ?? false,
     'search' => $search ?? true,
     'lib' => $lib ?? 'select2',
+    'theme' => $theme ?? 'bootstrap', // 'bootstrap' (default) or 'tailwind'
 ])
 
 <select
@@ -10,7 +11,7 @@
     name="{{ $attributes->get('name') }}"
     {{ $multiple ? 'multiple' : '' }}
     {{ $attributes->merge([
-        'class' => 'flash-select w-full',
+        'class' => trim('flash-select ' . ($theme === 'bootstrap' ? 'form-select w-full' : 'block w-full rounded-md border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50')),
         'data-flash-select' => true,
         'data-flash-select-lib' => $lib,
         'data-flash-select-search' => $search ? '1' : '0',
